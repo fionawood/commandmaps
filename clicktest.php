d860d44d2d64700cad3d67e498210c8587691533
Fiona Wood <fwood@college.harvard.edu>
2013-09-15 22:04:11 -0400

    new clicktst

---
+++ clicktest.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title> - jsFiddle demo</title>
+  
+  <script type='text/javascript' src='//code.jquery.com/jquery-1.9.1.js'></script>
+  
+  
+  
+  <link rel="stylesheet" type="text/css" href="/css/result-light.css">
+  
+  <style type='text/css' media="screen">
+  	canvas, img {display:block margin:lem auto; border:1px solid black;}
+  	canvas { background:url(http://monster.krash.net/cs279/commandmaps/gfx/commandmap.png);}
+ 
+  </style>
+  
+
+<script type='text/javascript'>//<![CDATA[ 
+
+$(window).load(function(){
+
+	jQuery(document).ready(function(){
+
+	     $("#special").click(function(e){ 
+
+	        var x = e.pageX - this.offsetLeft;
+	        var y = e.pageY - this.offsetTop; 
+
+	        $('#status2').html(x +', '+ y); 
+	   }); 
+	})  
+});//]]>  
+
+</script>
+
+
+</head>
+<body>
+  <body> 
+
+    <h2 id="status2">0, 0</h2>
+    <canvas width="800px" height="600px" style="width: 800px; height: 600px; border:1px ridge green;" id="special">
+
+    </canvas>
+
+</body>
+  
+</body>
+
+
+</html>
+
