3986e87db52d657fe525b6046d335640c7057107
Sharon Zhou <Zhou12@fas.harvard.edu>
2013-09-15 20:18:27 -0400

    stuff

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <link href="css/styles.css" rel="stylesheet"/>
+       
+       <?php if (isset($title)): ?>
+            <title>it's us. hello!<?= htmlspecialchars($title) ?></title>
+        <?php else: ?>
+            <title>CS279!</title>
+        <?php endif ?>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+    </head>
+
+    <body>
+
+        <div class="container-fluid">
+
+            <div id="middle">
