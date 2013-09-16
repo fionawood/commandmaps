273af428add9d98010e63bedd0267f2adb77cdc3
Daniel Haehn <haehn@seas.harvard.edu>
2013-09-15 21:57:25 -0400

    Add the user model.

---
+++ user.model.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'object.model.php';
+
+
+class User extends Object {
+
+  public $ip = '';
+
+  public $browser = '';
+
+  public $load_time = '';
+
+  public $device = '';
+
+  public $preference = '';
+
+  public $commends = '';
+
+  public $state = -1;
+
+}
+
+
+?>
