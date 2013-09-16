7b57b82223db54cf4f0c2c05d149e128f370d765
Fiona Wood <fwood@college.harvard.edu>
2013-09-15 22:08:47 -0400

    elaborating clicktest

--- clicktest.php
+++ clicktest.php
@@ -21,6 +21,35 @@
 <script type='text/javascript'>//<![CDATA[ 
 
 $(window).load(function(){
+	
+	var radius = 10;
+	var alpha = 1;
+	var x = 10;
+	var y = 10;
+
+
+	var canvas = document.getElementById('special');
+	var ctx = canvas.getContext('2d');
+		// ctx.clearRect(0,0,canvas.width,canvas.height);
+		// ctx.save();
+		// ctx.translate(canvas.width/2,canvas.height/2);
+	ctx.fillStyle = 'rgba(0,100,200,'+alpha+')';
+	ctx.beginPath();
+	ctx.arc(x,y,radius,0,Math.PI*2,false);
+	ctx.fill();
+	
+	if(radius >= 100) {
+		radius = 0;
+	} else {
+		radius+=1;
+	}
+	
+	if(alpha <= 0){
+		alpha=1;
+	} else {
+		alpha-=.01;
+	}
+		//ctx.restore();
 
 	jQuery(document).ready(function(){
 
