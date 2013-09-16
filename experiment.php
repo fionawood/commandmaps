b8cddac055af88f21d5c4318a3fe4acc60468b63
Sharon Zhou <Zhou12@fas.harvard.edu>
2013-09-16 00:11:43 -0400

    d

--- experiment.php
+++ experiment.php
@@ -2,7 +2,6 @@
 <?php 
 require_once('header.php');
 ?>
-<img class="interface-img">IMAGE GOES HERE</img>
 
 <div style="display:inline-block">
 	<div class="box-instr">Icon Name | Icon</div>
@@ -10,9 +9,7 @@ require_once('header.php');
 </div>
 
 ...after click on a non-menuTab, beep and display next button
-box
 
-icon and icon name
 
 <?php 
 require_once('footer.php');
