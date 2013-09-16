21074d5fe7780f5cccaa5e38f85e0e4d8d20e550
Sharon Zhou <Zhou12@fas.harvard.edu>
2013-09-15 21:24:51 -0400

    next btn

--- home.php
+++ home.php
@@ -10,9 +10,10 @@ Instructions:
 <ul>You will see an inset screen of something resembling Microsoft Word 2007.</ul>
 <ul>We will give you an icon as your target.</ul>
 <ul>Find and click on that target in the inset screen.</ul>
-<ul>Then click the <button>Next</button> button for the next icon, your new target.</ul>
+<ul>Then click the <button class="next-btn">Next</button> button for the next icon, your new target.</ul>
 </div>
 
-<h3>We have 2 interfaces for you to test out. First, some practice. Click the next button below to begin.</h3>
+We have 2 interfaces for you to test out! Some practice first. 
+</br><h3>Click the next button below to begin.</h3>
 
-<button>Next</button>
+<button class="next-btn">Next</button>
