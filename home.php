821ae7f2f0bb43cd3e14423602e36124da7adc4b
Sharon Zhou <Zhou12@fas.harvard.edu>
2013-09-15 20:49:35 -0400

    home

--- home.php
+++ home.php
@@ -1,2 +1,20 @@
 <!-- Welcome to the source code of team CS279 CommandMaps! -->
+<!DOCTYPE html>
+
 Home.
+<h1>Designing for You</h1>
+Help us study user interface design.
+</br></br>
+
+<div style="text-align:left;">
+Instructions:
+</br>
+<ul>You will see an inset screen of something resembling Microsoft Word 2007.</ul>
+<ul>We will give you an icon as your target.</ul>
+<ul>Find and click on that target in the inset screen.</ul>
+<ul>Then click the <button>Next</button> button for the next icon, your new target.</ul>
+</div>
+
+<h3>We have 2 interfaces for you to test out. First, some practice. Click the next button below to begin.</h3>
+
+<button>Next</button>
