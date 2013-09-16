69d4179a2b8a49e37f64eab5d655e91dd3fe76d4
Sharon Zhou <Zhou12@fas.harvard.edu>
2013-09-15 22:24:55 -0400

    questionnaire

--- questionnaire.php
+++ questionnaire.php
@@ -2,25 +2,28 @@
 require_once('header.php');
 ?>
 
-<div style="text-align:left">
+<div style="text-align:left;margin:5%;">
 <h3>Some questions:</h3>
-Which interface did you prefer?
+<b>Which interface did you prefer?</b>
 <form>
 <input type="radio" name="preference" value="ribbons">Ribbons<br>
 <input type="radio" name="preference" value="cmdmps">Commandmaps
 </form>
 </br>
-What device are you using on your computer?
+<b>What device are you using on your computer?</b>
 <form>
 <input type="radio" name="device" value="mouse">Mouse<br>
 <input type="radio" name="device" value="trackpad">Trackpad
 </form>
 </br>
-Comments?
-<div style="color:grey;font-size:small"><i>technical difficulties? thoughts in general?</i></div>
+<b>Comments?</b>
+<div style="color:grey;font-size:small"><i>technical difficulties? thoughts in general?</i></div></br>
+<form method="post" action="">
+<textarea name="comments" id="comments" cols="25" rows="5">
+</textarea><br><br>
+<button class="next-btn" type="submit" href="/finish.php" style="margin-left:5px;">Next</button>
+</form>
 </div>
-
-<button class="next-btn" href="finish.php">Next</button>
 
 <?php 
 require_once('footer.php');
