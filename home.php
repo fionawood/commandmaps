45f61ad784f900e7e653d80ce2597d5e4e0fddfe
Fiona Wood <fwood@college.harvard.edu>
2013-09-28 15:41:19 -0400

    Facebook share btw

--- home.php
+++ home.php
@@ -154,8 +154,12 @@
 	<h2>Thank you for participating in our study! We really appreciate your help.</h2>
 
     <h2>You were SSSSCCAAARRRYYY-FAST!!! <div id='ranking'>Ranking: #10/100</div></h2>
-    
-
-    <h2>Scare your friends! Send them this link: <a href="http://tinyurl.com/focusmusic" target="_blank">http://tinyurl.com/focusmusic</a></h2>
-    <a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Ftinyurl.com/focusmusic" target="_blank">Share on Facebook</a>
+    <script>
+    function fbShare(url, title, descr, image, winWidth, winHeight) {
+        var winTop = (screen.height / 2) - (winHeight / 2);
+        var winLeft = (screen.width / 2) - (winWidth / 2);
+        window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+    }
+    </script>
+    <a href="javascript:fbShare('http://tinyurl.com/focusmusic', 'Test your concentration!', 'How fast can you click?', 'gfx/picture.png', 520, 350)">Share</a>
 </div>
